87e1289f5da4f66160431ba60c531c3600f27d66
bmotadev <bmotadev@gmail.com>
2022-09-05 11:26:25 -0300

    Add project files.

---
+++ ArrayseListas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArrayseListas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Arrays
+            var numbersCopy = new int[10];
+            var numbers = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+
+            //0-based
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                numbersCopy[i] = numbers[i];
+            }
+            #endregion Arrays
+
+            #region LINQ
+            #endregion LINQ
+        }
+    }
+}
