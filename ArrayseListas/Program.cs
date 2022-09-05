f6630a4e6ea3799aa393e12a4f30f6c0a229e8d6
bmotadev <bmotadev@gmail.com>
2022-09-05 11:28:50 -0300

    Outra maneira para iniciar a matriz

--- ArrayseListas/Program.cs
+++ ArrayseListas/Program.cs
@@ -9,6 +9,7 @@ namespace ArrayseListas
             #region Arrays
             var numbersCopy = new int[10];
             var numbers = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            // int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
 
             //0-based
